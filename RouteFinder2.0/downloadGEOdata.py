126498e80344defa2ed8bcd36e34d586ff8a63ed
schmidtbit <schmidtabox@gmail.com>
2018-09-24 15:35:30 -0500

    2.0 version

---
+++ RouteFinder2.0/downloadGEOdata.py
@@ -0,0 +1,26 @@
+import requests, zipfile, io, os
+
+
+def Download_Unzip_GEOs(state):
+    '''
+    INPUT:  (1) state FIPS code
+            
+    OUTPUT: String; path to block level GEO data 
+    
+    ** This function downloads and extracts data to the './data' directory
+    '''
+    
+    # download zip file and extract to a local 'data' folder
+    url = 'https://www2.census.gov/geo/tiger/TIGER2017/TABBLOCK/' + 'tl_2017_{}_tabblock10.zip'.format(state)
+    r = requests.get(url)
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    new_path = './data/blockGEOs_{}'.format(state)
+    if not os.path.exists(new_path):
+        os.makedirs(new_path)
+    z.extractall(new_path)
+    
+    return new_path
+    
+if __name__ == "__main__":
+    state = str(input('enter state FIPS code: '))
+    Download_Unzip_GEOs(state) 
---
+++ RouteFinder2.0/myAPIs.py
@@ -0,0 +1,4 @@
+# My file for storing API keys
+
+def census_API_key():
+    return 'a662e90532ed3f245aa033dd19d53104d218bdc7'
